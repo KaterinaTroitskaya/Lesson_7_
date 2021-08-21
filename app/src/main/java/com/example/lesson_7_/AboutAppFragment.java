0bfd320f7a03ccd8971c5c4fa8e785ecee9c4add
kate <2951525@gmail.com>
2021-08-21 19:20:50 +0300

    Удалила ненужные директории, создала свой фрагмент "О приложении".

---
+++ app/src/main/java/com/example/lesson_7_/AboutAppFragment.java
@@ -0,0 +1,21 @@
+package com.example.lesson_7_;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class AboutAppFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_about_app, container, false);
+        return view;
+    }
+}
